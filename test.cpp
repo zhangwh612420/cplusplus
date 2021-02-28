edc17c6901babf2968f849d3ba6f892fab8919f9
zhangwh612420 <20212020086@fudan.edu>
2021-02-28 15:55:39 +0800

    test

--- test.cpp
+++ test.cpp
@@ -1,1 +1,19 @@
+/*
+* test.cpp
+* @author WenhaoZ
+* @description 
+* @created 2021-02-28T15:27:12.052Z+08:00
+* @copyright just a personal file
+* @last-modified 2021-02-28T15:55:16.833Z+08:00
+* @contact-email 20212020086@fudan.edu.cn
+*/
+
 // just for testing
+#include <iostream>
+int main()
+{
+    using namespace std;
+    cout << "HelloWorld\n";
+    cout << "2333\n";
+    return 0;
+}
