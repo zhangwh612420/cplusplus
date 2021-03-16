428f21766748d89dfa121a5e0056567364982206
zhangwh612420 <20212020086@fudan.edu>
2021-03-16 09:42:00 +0800

    test

--- cppprime/2.cpp
+++ cppprime/2.cpp
@@ -1,25 +1,13 @@
 #include <iostream>
+#include <vector>
+using std::cout;
+using std::endl;
+using std::vector;
 
 int main()
 {
-    int flagVal = 0, countNum = 0, currVal = 0;
-    if (std::cin >> flagVal)
-    {
-        countNum = 1;
-        while (std::cin >> currVal)
-        {
-            if (currVal == flagVal)
-            {
-                countNum += 1;
-            }
-            else
-            {
-                // just for test
-                std::cout << "This number " << flagVal << " occurs " << countNum << " times" << std::endl;
-                flagVal = currVal;
-                countNum = 1;
-            }
-        }
-        std::cout << "This number " << flagVal << " occurs " << countNum << " times" << std::endl;
-    }
+    vector<int> v1{1, 2};
+    auto vector_size = sizeof v1;
+    cout << vector_size << endl;
+    return 0;
 }
